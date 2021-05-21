fe903e02b929b6f5f8c0dfb8b5bffc1aaf1d059f
hanxie <hanxie.wq@alibaba-inc.com>
2021-05-21 19:17:45 +0800

    update info

--- myportal/src/App.js
+++ myportal/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test Edit <code>src/App.js</code> and save to reload.
+          Hello Serverless Devs
         </p>
         <a
           className="App-link"
